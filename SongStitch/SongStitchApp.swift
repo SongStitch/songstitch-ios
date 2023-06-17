19193d8858132716e136a0e25f12342c4b7bf95f
TheDen <deniskhoshaba@gmail.com>
2023-06-18 07:08:13 +1000

    Initial Commit

---
+++ SongStitch/SongStitchApp.swift
@@ -0,0 +1,17 @@
+//
+//  SongStitchApp.swift
+//  SongStitch
+//
+//  Created by TheDen on 18/6/2023.
+//
+
+import SwiftUI
+
+@main
+struct SongStitchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
